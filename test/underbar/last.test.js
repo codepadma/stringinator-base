06f84fe08f4f43285e7a9d7d49c8a1b69194d056
codepadma <padma.govindarajalu@gmail.com>
2017-05-09 11:40:14 -0700

    add testcases to test _.last

--- test/underbar/last.test.js
+++ test/underbar/last.test.js
@@ -2,14 +2,17 @@ const _ = require('../../underbar');
 
 describe('last()', () => {
   it('returns the last element of an array', () => {
-    // Your code goes here
+    const array = [100, 200, 300];
+    expect(_.last(array)).toBe(300);
   });
 
   it('returns the last 2 elements of an array', () => {
-    // Your code goes here
+    const array = [100, 200, 300];
+    expect(_.last(array, 2)).toEqual([200, 300]);
   });
 
   it('returns the whole array if you ask for more elements than it has', () => {
-    // Your code goes here
+    const array = [100, 200 ,300];
+    expect(_.last(array, 5)).toEqual([100, 200, 300]);
   });
 });

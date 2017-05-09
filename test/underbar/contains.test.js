4bee1e99b8fe8883e833d5262195a6ab028ce8cb
codepadma <padma.govindarajalu@gmail.com>
2017-05-09 13:29:37 -0700

    add testcases to test _.contains

--- test/underbar/contains.test.js
+++ test/underbar/contains.test.js
@@ -2,19 +2,33 @@ const _ = require('../../underbar');
 
 describe('contains()', () => {
   it('returns true if an array contains the target', () => {
-    // Your code goes here
+    const array= [200, 300, 400];
+    expect(_.contains(array, 300)).toBe(true);
   });
 
   it('returns false if an array does not contain the target', () => {
-    // Your code goes here
+    const array= [200, 300, 400];
+    expect(_.contains(array, 500)).toBe(false);
   });
 
   it('returns true if the target value is among the values of an object', () => {
-    // Your code goes here
+    const butterflies = {
+      butterfly1: 'monarch',
+      butterfly2: 'crescentspot',
+      butterfly3: 'aphrodite', 
+      butterfly4: 'viceroy'
+    };
+    expect(_.contains(butterflies, 'monarch')).toBe(true);
   });
 
   it('returns false if the target value is not among the values of an object', () => {
-    // Your code goes here
+    const butterflies = {
+      butterfly1: 'monarch',
+      butterfly2: 'crescentspot',
+      butterfly3: 'aphrodite', 
+      butterfly4: 'viceroy'
+    };
+    expect(_.contains(butterflies, 'isabella')).toBe(false);
   });
 
 });

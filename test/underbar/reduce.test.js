6dbd7b620cb5d1802db4f28ac51fd0f247989a97
codepadma <padma.govindarajalu@gmail.com>
2017-05-09 13:19:00 -0700

    add testcase to test reduce

--- test/underbar/reduce.test.js
+++ test/underbar/reduce.test.js
@@ -2,11 +2,13 @@ const _ = require('../../underbar');
 
 describe('reduce()', () => {
   it('reduces an array of numbers to a sum, *with* an explicit initial value for the accumulator', () => {
-    // Your code goes here
+    const array = [1, 2, 3, 4, 5];
+    expect(_.reduce(array, (accumulator, item) => accumulator + item, 0)).toBe(15);
   });
 
   it('reduces an array of numbers to a sum, *without* an explicit initial value for the accumulator', () => {
-    // Your code goes here
+    const array = [1, 2, 3, 4, 5];
+    expect(_.reduce(array, (accumulator, item) => accumulator + item)).toBe(15);
   });
 
 });

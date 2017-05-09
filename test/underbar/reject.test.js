acd6d81e93c2a849958b8561a423b186e280cb59
codepadma <padma.govindarajalu@gmail.com>
2017-05-09 14:02:46 -0700

    add testcases to test _.reject

--- test/underbar/reject.test.js
+++ test/underbar/reject.test.js
@@ -2,10 +2,16 @@ const _ = require('../../underbar');
 
 describe('reject()', () => {
   it('rejects odd numbers from an array', () => {
-    // Your code goes here
+    let nums = [2, 5, 7, 100, 50];
+    expect(_.reject(nums, item => item % 2 === 1)).toEqual([2, 100, 50]);
   });
 
   it('rejects null values from an object', () => {
-    // Your code goes here
+    const student = {
+      name: 'pinky',
+      age: 10,
+      dlno: null
+    };
+    expect(_.reject(student, item => item === null)).toEqual(['pinky', 10]);
   });
 });

fe15f95a69dcb10a4f9cd70bb55066ac07a2e059
codepadma <padma.govindarajalu@gmail.com>
2017-05-09 13:03:29 -0700

    add testcases to test _.each

--- test/underbar/each.test.js
+++ test/underbar/each.test.js
@@ -2,15 +2,40 @@ const _ = require('../../underbar');
 
 describe('each()', () => {
   it('iterates every element of an array, passing that element, its corresponding index, and the entire array to the callback', () => {
-    // Your code goes here
+    const array = [100, 200, 300];
+    let count = 0;
+    _.each(array, (item, index, array) => {
+      expect(array[index]).toBe(item);
+      count += 1;
+    });
+    expect(count).toBe(3);
   });
 
   it('iterates every element of an array-like object, passing that element, its corresponding index, and the entire array to the callback', () => {
-    // Your code goes here
+    const arrayLikeObj = {
+      0: 'Charlie',
+      1: 9,
+      length: 2
+    };
+    let count = 0;
+    _.each(arrayLikeObj, (item, index, iteratedObj) => {
+      expect(iteratedObj[index]).toBe(item);
+      count += 1;
+    });
+    expect(count).toBe(2);
   });
 
   it('iterates every property of an object, passing the value, the corresponding key, and the entire object to the callback', () => {
-    // Your code goes here
+    let count = 0;
+    const obj = {
+      name: 'Charlie',
+      age: 9
+    };
+    _.each(obj, (item, key, iteratedObj) => {
+      expect(iteratedObj[key]).toBe(item);
+      count += 1;
+    });
+    expect(count).toBe(2);
   });
 });
 

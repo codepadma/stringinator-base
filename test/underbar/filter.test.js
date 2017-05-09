eca5625489fc1897267384f90c0dcc4ecdff33d6
codepadma <padma.govindarajalu@gmail.com>
2017-05-09 13:54:25 -0700

    add testcase to test _.filter

--- test/underbar/filter.test.js
+++ test/underbar/filter.test.js
@@ -2,10 +2,12 @@ const _ = require('../../underbar');
 
 describe('filter()', () => {
   it('filters an array to odd numbers', () => {
-    // Your code goes here
+    const nums = [200, 405, 300, 515];
+    expect(_.filter(nums, item => item % 2 === 1 )).toEqual([405, 515]);
   });
 
   it('filters an object to only numeric values', () => {
-    // Your code goes here
+    const obj = {name: 'Bob', age: 30};
+    expect(_.filter(obj, item => typeof(item) === 'number')).toEqual([30]);
   });
 });

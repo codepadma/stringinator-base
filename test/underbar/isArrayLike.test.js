a880b82ede57f30949045f7a41b376724709de1e
codepadma <padma.govindarajalu@gmail.com>
2017-05-09 12:24:07 -0700

    add testcases to test _.isArraylike

--- test/underbar/isArrayLike.test.js
+++ test/underbar/isArrayLike.test.js
@@ -2,14 +2,24 @@ const _ = require('../../underbar');
 
 describe('isArrayLike()', () => {
   it('returns true for an actual array', () => {
-    // Your code goes here
+    const array = [1, 2, 3, 4, 5];
+    expect(_.isArrayLike(array)).toBe(true);
   });
 
   it('returns true for an array-like object', () => {
-    // Your code goes here
+    const arrayLikeObj = {
+      '0': 'Charlie',
+      '1': 9,
+      length: 2
+    };
+    expect(_.isArrayLike(arrayLikeObj)).toBe(true);
   });
 
   it('returns false for a non-array-like object', () => {
-    // Your code goes here
+    const nonArrayLikeObj = {
+      'name': 'Charlie',
+      'age': 9
+    };
+    expect(_.isArrayLike(nonArrayLikeObj)).toBe(false);
   });
 });

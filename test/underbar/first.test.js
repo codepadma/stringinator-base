294402fc5ce037fd72b4850c35442edcb391ba50
codepadma <padma.govindarajalu@gmail.com>
2017-05-09 11:37:31 -0700

    add testcases to test _.first

--- test/underbar/first.test.js
+++ test/underbar/first.test.js
@@ -2,14 +2,17 @@ const _ = require('../../underbar');
 
 describe('first()', () => {
   it('returns the first element of an array', () => {
-    // Your code goes here
+    const array = [100, 200, 300];
+    expect(_.first(array)).toBe(100);
   });
 
   it('returns the first 2 elements of an array', () => {
-    // Your code goes here
+    const array = [100, 200, 300];
+    expect(_.first(array, 2)).toEqual([100, 200]);
   });
 
   it('returns the whole array if you ask for more elements than it has', () => {
-    // Your code goes here
+    const array = [100, 200, 300];
+    expect(_.first(array, 5)).toEqual([100, 200, 300]);
   });
 });

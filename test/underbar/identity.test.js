1b68ab227043829dec1b7dfc3037edd06672958d
codepadma <padma.govindarajalu@gmail.com>
2017-05-09 11:32:33 -0700

    add testcases to test _.identity function with input values such as null, number, array, object

--- test/underbar/identity.test.js
+++ test/underbar/identity.test.js
@@ -2,18 +2,22 @@ const _ = require('../../underbar');
 
 describe('identity()', () => {
   it('returns null if given null', () => {
-    // Your code goes here
+    expect(_.identity(null)).toBe(null);
   });
 
   it('returns the number if given a number', () => {
-    // Your code goes here
+    expect(_.identity(5)).toBe(5);
   });
 
   it('returns the same array if given an array', () => {
-    // Your code goes here
+    expect(_.identity([2, 3, 4, 5])).toEqual([2, 3, 4, 5]);
   });
 
   it('returns the same object if given an object', () => {
-    // Your code goes here
+    const obj = {'name': 'Monarch Butterfly', 
+                 'family': 'Nymphalidae',
+                 'region': 'North America'
+                };
+    expect(_.identity(obj)).toBe(obj);
   });
 });
